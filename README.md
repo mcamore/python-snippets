51f677c6ba3005df721599121c620bb9f4180c03
mcamore <mmco94@live.com>
2019-10-27 13:21:17 +0100

    Fibonacci script and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# python
+# Python
+
+Just scripts I made for fun ¯\_(ツ)_/¯
+Specifications and usage are commented inside every script, enjoy! 
+
+
---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+# Author: mcamore (mmco94@live.com)
+# Usage: python fibonacci.py [numer_of_iterations]
+# Example: "python fibonnaci.py 5" returns "[0, 1, 1, 2, 3, 5]"
+# Requires Python 3.x
+
+from sys import argv
+def fibonacci():
+	fibonacciList = [0, 1]
+	try:
+		for i in range(1, int(argv[1])):
+			sumatory = fibonacciList[i - 1] + fibonacciList[i]
+			fibonacciList.append(sumatory)
+		print(fibonacciList)
+	except IndexError:
+		print("\nError: Introduce the number of iterations.\nExample: fibonacci.py 10")
+	except ValueError:
+		print("\nError: Number of iterations must be a integer value.\nExample: fibonacci.py 10")
+	except:
+		print("\nUknown error...")
+
+if __name__ == "__main__": fibonacci()
